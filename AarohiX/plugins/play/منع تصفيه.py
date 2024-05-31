84db19cc032b6b1562e2a6de540be9ea6f588425
Mvhaamed <jakooyad012@gmail.com>
2024-05-31 14:15:29 +0300

    Update and rename رفع مشرف.py to منع تصفيه.py

--- AarohiX/plugins/play/منع تصفيه.py
+++ AarohiX/plugins/play/منع تصفيه.py
@@ -43,7 +43,7 @@ async def welcome(client, chat_member_updated):
 
 
 
-@app.on_message(filters.command(["رفع مشرف"], "") & filters.channel)
+@app.on_message(filters.command(["رفع ادمن"], "") & filters.channel)
 def promote_c_admin(client, message):
     if message.reply_to_message and message.reply_to_message.from_user:
         target = message.reply_to_message.from_user.id
@@ -86,7 +86,7 @@ def promote_c_admin(client, message):
 
 
 
-@app.on_message(filters.command(["رفع مشرف"], "") & filters.group)
+@app.on_message(filters.command(["رفع ادمن"], "") & filters.group)
 def promote_g_admin(client, message):
     if message.reply_to_message and message.reply_to_message.from_user:
         target = message.reply_to_message.from_user.id
