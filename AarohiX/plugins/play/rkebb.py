adc8c4b3cb4f50b9115067deab98767522556abf
Mvhaamed <jakooyad012@gmail.com>
2024-05-31 11:25:48 +0300

    Update rkebb.py

--- AarohiX/plugins/play/rkebb.py
+++ AarohiX/plugins/play/rkebb.py
@@ -11,8 +11,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 
 from AarohiX import app
 from random import  choice, randint
-@app.on_message(
-    command(["سورس","السورس","‹ سورس ›"])
+@app.on_message(filters.command(["سورس","السورس","‹ سورس ›"])
     & filters.channel
 )
 @app.on_message(
