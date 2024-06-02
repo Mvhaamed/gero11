82b08c725bd69aab841b505da08f800214ec1cdd
Mvhaamed <jakooyad012@gmail.com>
2024-06-03 00:13:55 +0300

    Update thumbnails.py

--- AarohiX/utils/thumbnails.py
+++ AarohiX/utils/thumbnails.py
@@ -1,22 +1,15 @@
-#
-# Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
+
 import os
 import re
 import textwrap
-
+from typing import Union
 import aiofiles
 import aiohttp
-import numpy as np
-from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
+from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
+                 ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
 
-from config import YOUTUBE_IMG_URL
+from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 from AarohiX import app
 
 
@@ -27,25 +20,13 @@ def changeImageSize(maxWidth, maxHeight, image):
     newHeight = int(heightRatio * image.size[1])
     newImage = image.resize((newWidth, newHeight))
     return newImage
+ahmed = ""
 
-
-def circle(img):
-    h, w = img.size
-    a = Image.new("L", [h, w], 0)
-    b = ImageDraw.Draw(a)
-    b.pieslice([(0, 0), (h, w)], 0, 360, fill=255, outline="white")
-    c = np.array(img)
-    d = np.array(a)
-    e = np.dstack((c, d))
-    return Image.fromarray(e)
-
-
-async def get_thumb(videoid, photo):
-    if os.path.isfile(f"{photo}.png"):
-        return f"{photo}.png"
-
-    url = f"https://www.youtube.com/watch?v={videoid}"
-    try:
+async def gen_thumb(videoid, photo):
+   try:
+        if os.path.isfile(f"{videoid}{photo}.png"):
+          return f"{videoid}{photo}.png"
+        url = f"https://www.youtube.com/watch?v={videoid}"
         results = VideosSearch(url, limit=1)
         for result in (await results.next())["result"]:
             try:
@@ -71,25 +52,30 @@ async def get_thumb(videoid, photo):
         async with aiohttp.ClientSession() as session:
             async with session.get(thumbnail) as resp:
                 if resp.status == 200:
-                    f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
+                    f = await aiofiles.open(
+                        f"cache/thumb{videoid}.png", mode="wb"
+                    )
                     await f.write(await resp.read())
                     await f.close()
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        Yukki = Image.open(f"{photo}")
+        try:
+          elnqybv = Image.open(f"{photo}")
+        except:
+          elnqybv = Image.open(f"cache/thumb{videoid}.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(30))
+        background = image2.filter(filter=ImageFilter.BoxBlur(5))
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.6)
-        Xcenter = Yukki.width / 2
-        Ycenter = Yukki.height / 2
+        Xcenter = elnqybv.width / 2
+        Ycenter = elnqybv.height / 2
         x1 = Xcenter - 250
         y1 = Ycenter - 250
         x2 = Xcenter + 250
         y2 = Ycenter + 250
-        logo = Yukki.crop((x1, y1, x2, y2))
-        logo.thumbnail((520, 520), Image.LANCZOS)
+        logo = elnqybv.crop((x1, y1, x2, y2))
+        logo.thumbnail((520, 520), Image.ANTIALIAS)
         logo = ImageOps.expand(logo, border=15, fill="white")
         background.paste(logo, (50, 100))
         draw = ImageDraw.Draw(background)
@@ -99,10 +85,9 @@ async def get_thumb(videoid, photo):
         name_font = ImageFont.truetype("AarohiX/assets/font.ttf", 30)
         para = textwrap.wrap(title, width=32)
         j = 0
-        draw.text((5, 5), f"Alina Music The Most Advanced Telegram Music Bot", fill="white", font=name_font)
         draw.text(
             (600, 150),
-            "ALINA PLAYING",
+            "TEST PlAYiNg",
             fill="white",
             stroke_width=2,
             stroke_fill="white",
@@ -148,131 +133,8 @@ async def get_thumb(videoid, photo):
             (255, 255, 255),
             font=arial,
         )
-        try:
-            os.remove(f"{photo}")
-            os.remove(f"thumb{videoid}.png")
-        except:
-            pass
-        background.save(f"{photo}.png")
-        return f"{photo}.png"
-    except Exception:
-        return YOUTUBE_IMG_URL
-
-
-async def gen_qthumb(videoid):
-    if os.path.isfile(f"{photo}.png"):
-        return f"{photo}.png"
-
-    url = f"https://www.youtube.com/watch?v={videoid}"
-    try:
-        results = VideosSearch(url, limit=1)
-        for result in (await results.next())["result"]:
-            try:
-                title = result["title"]
-                title = re.sub("\W+", " ", title)
-                title = title.title()
-            except:
-                title = "Unsupported Title"
-            try:
-                duration = result["duration"]
-            except:
-                duration = "Unknown Mins"
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-            try:
-                views = result["viewCount"]["short"]
-            except:
-                views = "Unknown Views"
-            try:
-                channel = result["channel"]["name"]
-            except:
-                channel = "Unknown Channel"
-
-        async with aiohttp.ClientSession() as session:
-            async with session.get(thumbnail) as resp:
-                if resp.status == 200:
-                    f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
-                    await f.write(await resp.read())
-                    await f.close()
-
-        youtube = Image.open(f"cache/thumb{videoid}.png")
-        Yukki = Image.open(f"{photo}")
-        image1 = changeImageSize(1280, 720, youtube)
-        image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(30))
-        enhancer = ImageEnhance.Brightness(background)
-        background = enhancer.enhance(0.6)
-        Xcenter = Yukki.width / 2
-        Ycenter = Yukki.height / 2
-        x1 = Xcenter - 250
-        y1 = Ycenter - 250
-        x2 = Xcenter + 250
-        y2 = Ycenter + 250
-        logo = Yukki.crop((x1, y1, x2, y2))
-        logo.thumbnail((520, 520), Image.LANCZOS)
-        logo = ImageOps.expand(logo, border=15, fill="white")
-        background.paste(logo, (50, 100))
-        draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("assets/font2.ttf", 40)
-        font2 = ImageFont.truetype("assets/font2.ttf", 67)
-        arial = ImageFont.truetype("assets/font2.ttf", 30)
-        name_font = ImageFont.truetype("assets/font.ttf", 30)
-        para = textwrap.wrap(title, width=32)
-        j = 0
-        draw.text((5, 5), f"Alina Music The Most Advanced Telegram Music Bot", fill="white", font=name_font)
-        draw.text(
-            (600, 150),
-            "ADDED TO QUEUE",
-            fill="white",
-            stroke_width=2,
-            stroke_fill="white",
-            font=font2,
-        )
-        for line in para:
-            if j == 1:
-                j += 1
-                draw.text(
-                    (600, 340),
-                    f"{line}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-            if j == 0:
-                j += 1
-                draw.text(
-                    (600, 280),
-                    f"{line}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-
-        draw.text(
-            (600, 450),
-            f"Views : {views[:23]}",
-            (255, 255, 255),
-            font=arial,
-        )
-        draw.text(
-            (600, 500),
-            f"Duration : {duration[:23]} Mins",
-            (255, 255, 255),
-            font=arial,
-        )
-        draw.text(
-            (600, 550),
-            f"Channel : {channel}",
-            (255, 255, 255),
-            font=arial,
-        )
-        try:
-            os.remove(f"{photo}")
-            os.remove(f"thumb{videoid}.png")
-        except:
-            pass
-        background.save(f"{photo}.png")
-        return f"{photo}.png"
-    except Exception:
-        return YOUTUBE_IMG_URL
+        background.save(f"{videoid}{photo}.png")
+        return f"{videoid}{photo}.png"
+   except Exception as a:
+        print(a)
+        return ahmed
