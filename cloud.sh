9c1663e9ecfc71e69dc973e615457696a671efef
Mvhaamed <jakooyad012@gmail.com>
2024-07-02 08:59:49 +0300

    Add files via upload

---
+++ cloud.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cat <<'EOF'
+---------------------------------------
+Installing Tepthon is in progress..                                                  
+---------------------------------------                                                  
+
+
+                                                  
+Copyright (C) 2020-2024 by Mvhaamed@Github, < https://github.com/Mvhaamed >.
+This file is part of < https://github.com/Mvhaamed/gero11 > project,
+and is released under the "GNU v3.0 License Agreement".
+Please see < https://github.com/Mvhaamed/gero11/blob/main/LICENSE >
+All rights reserved.
+EOF
+
+gunicorn app:app --daemon && python -m AarohiX
